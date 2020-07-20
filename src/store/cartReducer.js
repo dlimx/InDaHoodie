ce7512af8e7f0300a6235eb138d8511324eb7c4f
David <david.minxiao.li@gmail.com>
2020-07-20 00:37:07 -0700

    add product details and add to cart

--- src/store/cartReducer.js
+++ src/store/cartReducer.js
@@ -1,7 +1,49 @@
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  RESET_CART,
+  UPDATE_CART,
+} from './cartActions';
+
 export const initialState = {
   items: [],
 };
 
 export default function cartReducer(state = initialState, action) {
-  return state;
+  const { type, payload } = action;
+
+  switch (type) {
+    case ADD_TO_CART: {
+      return {
+        ...state,
+        items: [...state.items, { ...payload }],
+      };
+    }
+    case REMOVE_FROM_CART: {
+      return {
+        ...state,
+        items: state.items.filter((item) => item.id !== payload.id),
+      };
+    }
+    case UPDATE_CART: {
+      const index = state.items.findIndex((item) => item.id === payload.id);
+      const newItem = {
+        ...state.items[index],
+        ...payload,
+      };
+      const newItems = [...state.items];
+      newItems[index] = newItem;
+      return {
+        ...state,
+        items: newItems,
+      };
+    }
+    case RESET_CART: {
+      return {
+        ...initialState,
+      };
+    }
+    default:
+      return state;
+  }
 }
