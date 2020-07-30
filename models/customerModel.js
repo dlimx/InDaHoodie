26b9ee09f82fd71d948a69adbef72325a55a3fde
David <david.minxiao.li@gmail.com>
2020-07-29 22:18:32 -0700

    add customer model (#15)

---
+++ models/customerModel.js
@@ -0,0 +1,36 @@
+const db = require('./db');
+
+const getAll = async () => {
+  return db.pool.asyncQuery('SELECT * FROM Customers');
+};
+
+const getCustomerById = async (id) => {
+  const data = await db.pool.asyncQuery(
+    'SELECT * FROM Customers WHERE id = ?',
+    [id],
+  );
+  const user = data[0];
+  return user;
+};
+
+const createCustomer = async (data) => {
+  await db.pool.asyncQuery(
+    'INSERT INTO Customers (first_name, last_name, address, city, state, zip, image, birthdate, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, NOW(), NOW())',
+    [
+      data.first_name,
+      data.last_name,
+      data.address,
+      data.city,
+      data.state,
+      data.zip,
+      data.image,
+      data.birthdate,
+    ],
+  );
+};
+
+module.exports = {
+  getAll,
+  getCustomerById,
+  createCustomer,
+};
