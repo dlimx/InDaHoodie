f01b054d4a0199a34f88bdec18674566894f631f
blakelester <lesterst@oregonstate.edu>
2020-08-03 13:17:44 -0600

    Added UPDATE/DELETE functionality for Products table. Added CREATE/DELETE functionality for Products_Categories table.

--- models/productModel.js
+++ models/productModel.js
@@ -1,4 +1,5 @@
 const db = require('./db');
+const productCategoryModel = require('./productCategoryModel');
 
 const getAll = async () => {
   return db.pool.asyncQuery('SELECT * FROM Products');
@@ -23,18 +24,45 @@ const createProduct = async (data) => {
       data.designer_id, // not entirely sure if we'll be passing this value from our front-end
     ],
   );
+  const savedProduct = await db.pool.asyncQuery(
+    'SELECT * FROM Products WHERE id = (SELECT MAX(id) FROM Products)', // or... WHERE id = (SELECT LAST_INSERT_ID())
+  );
+  const productCategory = {
+    product_id: savedProduct[0].id,
+    category_id: data.category_id,
+  };
+  await productCategoryModel.createProductCategory(productCategory);
+  return savedProduct[0];
+};
+
+// ASSUMPTION: we're passing from front-end all product values (modified or not) with UPDATE request
+const updateProduct = async (data) => {
   await db.pool.asyncQuery(
-    'INSERT INTO Products_Categories (product_id, category_id) VALUES ((SELECT LAST_INSERT_ID()), ?)',
-    [data.category_id],
+    'UPDATE Products SET name=?, description=?, updated_at=CURDATE(), image=?, price=?, designer_id=? WHERE id=?',
+    [
+      data.name,
+      data.description,
+      data.image,
+      data.price,
+      data.designer_id,
+      data.id,
+    ],
   );
   const result = await db.pool.asyncQuery(
-    'SELECT * FROM Products WHERE id = (SELECT MAX(id) FROM Products)',
+    'SELECT * FROM Products WHERE id = ?',
+    [data.id],
   );
   return result[0];
+};
+
+const deleteProduct = async (id) => {
+  await db.pool.asyncQuery('DELETE FROM Products WHERE id = ?', [id]);
 };
 
 module.exports = {
   getAll,
   getProductById,
   createProduct,
+  updateProduct,
+  deleteProduct,
 };
