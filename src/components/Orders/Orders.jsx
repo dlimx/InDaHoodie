4738579d59508379121640b93b8fbd480f4c6f41
David <david.minxiao.li@gmail.com>
2020-08-14 02:02:19 -0400

    add small etc fixes (#28)

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,13 +3,16 @@ import { useHistory } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import api from '../../api/api';
 import OrderCard from '../shared/OrderCard';
+import Loading from '../shared/Loading';
 
 export default function Orders() {
+  const [loading, setLoading] = useState(true);
   const [orders, setOrders] = useState([]);
 
   useEffect(() => {
     api.get('/order').then(({ data }) => {
       setOrders(data);
+      setLoading(false);
     });
   }, []);
 
@@ -22,6 +25,7 @@ export default function Orders() {
         To create a new order, add items into the cart and then save it as an
         order.
       </p>
+      {loading && <Loading />}
       {orders.map((order, index) => (
         <OrderCard order={order} key={order.id} />
       ))}
