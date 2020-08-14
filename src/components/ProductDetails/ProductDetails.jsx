12fa3b46d28c7510b8de29ac7f637eb8fd0aa2a8
David <david.minxiao.li@gmail.com>
2020-08-14 01:53:50 -0400

    add small etc fixes

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -7,18 +7,21 @@ import './ProductDetails.css';
 import { useCart } from '../../store/CartProvider';
 import { addToCart } from '../../store/cartActions';
 import { getPrice } from '../../util/util';
+import Loading from '../shared/Loading';
 
 export default function ProductDetails() {
   const { id } = useParams();
   const history = useHistory();
   const [cart, dispatchCart] = useCart();
   const [product, setProduct] = useState({});
-  const [quantity, setQuantity] = useState('');
+  const [quantity, setQuantity] = useState();
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     api.get(`/product/${id}`).then(({ data }) => {
       setProduct(data);
+      setLoading(false);
     });
   }, [id]);
 
@@ -67,16 +70,12 @@ export default function ProductDetails() {
 
   const onDelete = (e) => {
     e.preventDefault();
-    console.log('hi');
     api
       .delete(`/product/delete/${id}`)
       .then(() => {
-        console.log('hi');
         history.push('/');
       })
-      .catch(() => {
-        console.log('hi');
-      });
+      .catch(() => {});
   };
 
   return (
@@ -87,71 +86,82 @@ export default function ProductDetails() {
       <div className="card ProductDetailsImage">
         <Icon icon="fa-camera" className="fa-5x" />
       </div>
-      <div className="ProductDetailsText">
-        <div className="Row RowSpaced">
-          <h2>{product.name}</h2>
-        </div>
-        <strong>${getPrice(product.price)}</strong>
-        <div className="Row">
-          <button type="button" onClick={onEdit} className="btn btn-outline">
-            <Icon icon="fa-edit" className="fa-lg" />
-          </button>
-          <button type="button" onClick={onDelete} className="btn btn-outline">
-            <Icon icon="fa-trash" className="fa-lg" style={{ color: 'red' }} />
-          </button>
-        </div>
-        <br />
-        <p>{product.description}</p>
-        <br />
-        {product.designer?.name ? <span>{product.designer.name}</span> : null}
-        <br />
-        {product.categories?.length ? (
-          <span>
-            {product.categories.map((category) => category.name).join(', ')}
-          </span>
-        ) : null}
-        <br />
-        <br />
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
+      {loading && <Loading />}
+      {!loading && (
+        <div className="ProductDetailsText">
+          <div className="Row RowSpaced">
+            <h2>{product.name}</h2>
+          </div>
+          <strong>${getPrice(product.price)}</strong>
+          <div className="Row">
+            <button type="button" onClick={onEdit} className="btn btn-outline">
+              <Icon icon="fa-edit" className="fa-lg" />
+            </button>
             <button
-              onClick={onDecrease}
               type="button"
-              className={`input-group-text ${
-                quantity === 0 ? 'ProductDetailsDisabled' : ''
-              }`}
+              onClick={onDelete}
+              className="btn btn-outline"
             >
-              -
+              <Icon
+                icon="fa-trash"
+                className="fa-lg"
+                style={{ color: 'red' }}
+              />
             </button>
           </div>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Quantity"
-            aria-label="Quantity"
-            value={quantity}
-            onChange={onChange}
-          />
-          <div className="input-group-append">
-            <button
-              onClick={onIncrease}
-              type="button"
-              className="input-group-text"
-            >
-              +
-            </button>
+          <br />
+          <p>{product.description}</p>
+          <br />
+          {product.designer?.name ? <span>{product.designer.name}</span> : null}
+          <br />
+          {product.categories?.length ? (
+            <span>
+              {product.categories.map((category) => category.name).join(', ')}
+            </span>
+          ) : null}
+          <br />
+          <br />
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <button
+                onClick={onDecrease}
+                type="button"
+                className={`input-group-text ${
+                  quantity === 0 ? 'ProductDetailsDisabled' : ''
+                }`}
+              >
+                -
+              </button>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Quantity"
+              aria-label="Quantity"
+              value={quantity}
+              onChange={onChange}
+            />
+            <div className="input-group-append">
+              <button
+                onClick={onIncrease}
+                type="button"
+                className="input-group-text"
+              >
+                +
+              </button>
+            </div>
           </div>
+          <p className="text-danger">{error}</p>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={onSubmit}
+            disabled={!quantity || error}
+          >
+            Add to Cart
+          </button>
         </div>
-        <p className="text-danger">{error}</p>
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={onSubmit}
-          disabled={!quantity || error}
-        >
-          Add to Cart
-        </button>
-      </div>
+      )}
     </div>
   );
 }
